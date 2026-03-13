19afb67553077fe210cd72ad03a7e4d066909b43
TanushKrashak <tankrashak38816@gmail.com>
2026-03-13 22:20:18 +0530

    added a new grid types enum for grid inventory

---
+++ Plugins/MultiplayerInventory/Source/MultiplayerInventory/Public/Types/Cpp_GridTypes.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "Cpp_GridTypes.generated.h"
+
+UENUM(BlueprintType)
+enum class EItemCategory : uint8 {
+	None,
+	Equippable,
+	Consumable,
+	Craftable,
+};
---
+++ Plugins/MultiplayerInventory/Source/MultiplayerInventory/Public/Widgets/Inventory/Spatial/Cpp_WGT_InventoryGrid.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Types/Cpp_GridTypes.h"
+#include "Cpp_WGT_InventoryGrid.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERINVENTORY_API UCpp_WGT_InventoryGrid : public UUserWidget {
+	GENERATED_BODY()
+	
+protected:
+	//=================================================================================================================
+	// FUNCTIONS
+	//=================================================================================================================
+	FORCEINLINE EItemCategory GetItemCategory() const { return ItemCategory; }
+	
+	//=================================================================================================================
+	// PROPERTIES & VARIABLES
+	//=================================================================================================================
+	UPROPERTY(EditAnywhere, Category="Inventory")
+	EItemCategory ItemCategory; 	
+};
