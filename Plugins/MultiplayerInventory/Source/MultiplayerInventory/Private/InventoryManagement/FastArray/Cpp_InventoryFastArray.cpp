214e58f5876e3f03ca83995fb459c3edbca1c30c
TanushKrashak <tankrashak38816@gmail.com>
2026-03-20 23:02:03 +0530

    added fast array func declarations

--- Plugins/MultiplayerInventory/Source/MultiplayerInventory/Private/InventoryManagement/FastArray/Cpp_InventoryFastArray.cpp
+++ Plugins/MultiplayerInventory/Source/MultiplayerInventory/Private/InventoryManagement/FastArray/Cpp_InventoryFastArray.cpp
@@ -1,1 +1,28 @@
 ﻿#include "InventoryManagement/FastArray/Cpp_InventoryFastArray.h"
+
+#include "Items/Cpp_InventoryItem.h"
+
+TArray<UCpp_InventoryItem> FInventoryFastArray::GetAllItems() const {
+	return TArray<UCpp_InventoryItem>();
+}
+
+void FInventoryFastArray::PreReplicatedRemove(const TArrayView<int32> RemovedIndices, int32 FinalSize) {
+}
+
+void FInventoryFastArray::PostReplicatedAdd(const TArrayView<int32> AddedIndices, int32 FinalSize) {
+}
+
+UCpp_InventoryItem* FInventoryFastArray::AddEntry(UCpp_AC_Inventory* ItemComponent) {
+	return nullptr;
+}
+
+UCpp_InventoryItem* FInventoryFastArray::AddEntry(UCpp_InventoryItem* Item) {
+	return nullptr;
+}
+
+void FInventoryFastArray::RemoveEntry(UCpp_InventoryItem* Item) {
+}
+
+bool FInventoryFastArray::NetDeltaSerialize(FNetDeltaSerializeInfo& DeltaParms) {
+	return FastArrayDeltaSerialize<FInventoryEntry, FInventoryFastArray>(Entries, DeltaParms, *this);
+}
--- Plugins/MultiplayerInventory/Source/MultiplayerInventory/Public/InventoryManagement/FastArray/Cpp_InventoryFastArray.h
+++ Plugins/MultiplayerInventory/Source/MultiplayerInventory/Public/InventoryManagement/FastArray/Cpp_InventoryFastArray.h
@@ -5,6 +5,7 @@
 
 #include "Cpp_InventoryFastArray.generated.h"
 
+class UCpp_AC_Inventory;
 class UCpp_InventoryItem;
 
 // single entry in inventory
@@ -27,4 +28,37 @@ USTRUCT()
 struct FInventoryFastArray : public FFastArraySerializer {
 	GENERATED_BODY()
 	
+	FInventoryFastArray() {
+		OwnerComponent = nullptr;
+	}
+	FInventoryFastArray(UActorComponent* InOwnerComponent) {
+		OwnerComponent = InOwnerComponent;
+	}
+	
+	TArray<UCpp_InventoryItem> GetAllItems() const;
+	
+	// FFastArray funcs
+	void PreReplicatedRemove(const TArrayView<int32> RemovedIndices, int32 FinalSize);
+	void PostReplicatedAdd(const TArrayView<int32> AddedIndices, int32 FinalSize);
+	bool NetDeltaSerialize(FNetDeltaSerializeInfo& DeltaParms);
+	
+	UCpp_InventoryItem* AddEntry(UCpp_AC_Inventory* ItemComponent);
+	UCpp_InventoryItem* AddEntry(UCpp_InventoryItem* Item);
+	void RemoveEntry(UCpp_InventoryItem* Item);
+	
+private:
+	friend UCpp_AC_Inventory; 
+	
+	UPROPERTY(NotReplicated)
+	TObjectPtr<UActorComponent> OwnerComponent;
+	
+	UPROPERTY()
+	TArray<FInventoryEntry> Entries;
+};
+
+template<>
+struct TStructOpsTypeTraits<FInventoryFastArray> : public TStructOpsTypeTraitsBase2<FInventoryFastArray> {
+	enum {
+		WithNetDeltaSerializer = true
+	};	
 };
