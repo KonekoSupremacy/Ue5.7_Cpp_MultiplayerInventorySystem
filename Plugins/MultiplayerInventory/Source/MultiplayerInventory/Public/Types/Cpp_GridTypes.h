480d680c4e73a72501aebdbddb0c7785438da627
TanushKrashak <tankrashak38816@gmail.com>
2026-03-23 22:39:53 +0530

    Created new slot avaibility structs

--- Plugins/MultiplayerInventory/Source/MultiplayerInventory/Public/Types/Cpp_GridTypes.h
+++ Plugins/MultiplayerInventory/Source/MultiplayerInventory/Public/Types/Cpp_GridTypes.h
@@ -2,10 +2,45 @@
 
 #include "Cpp_GridTypes.generated.h"
 
+class UCpp_InventoryItem;
+
 UENUM(BlueprintType)
 enum class EItemCategory : uint8 {
 	None,
 	Equippable,
 	Consumable,
 	Craftable,
+};
+
+USTRUCT()
+struct FSlotAvailability {
+	GENERATED_BODY()
+	
+	FSlotAvailability() {};
+	FSlotAvailability(const int16 InIndex, const int16 InAmount, const bool bInItemAtIndex) {
+		Index = InIndex;
+		AmountToFill = InAmount;
+		bItemAtIndex = bInItemAtIndex;
+	}
+	
+	int16 Index = -1;
+	int16 AmountToFill = 0;
+	bool bItemAtIndex = false;
+};
+
+USTRUCT()
+struct FSlotAvailabilityResult {
+	GENERATED_BODY()
+	
+	FSlotAvailabilityResult() {};
+	
+	UPROPERTY()
+	TWeakObjectPtr<UCpp_InventoryItem> Item;
+	
+	int16 TotalAmountToFill = 0;
+	int16 Remaining = 0;
+	
+	bool bStackable = false;
+	
+	TArray<FSlotAvailability> AvailableSlots;
 };
